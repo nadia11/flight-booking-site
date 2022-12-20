d73d42b9b6e8065a9004ea12f0906489c1fcf137
Nadia Tabassum <shormy.992@gmail.com>
2022-12-20 19:57:56 +0600

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+<Navbar/>
+
     </div>
   );
 }
